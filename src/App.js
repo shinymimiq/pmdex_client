fca1604f0d228505e097a984c8dd312fa1ceb250
Tony Liu <mrtoniliu@gmail.com>
2021-04-03 16:35:14 +0100

    Update

--- src/App.js
+++ src/App.js
@@ -6,49 +6,41 @@ import PokemonList from "./Components/PokemonList";
 
 const Pokedex = require("pokeapi-js-wrapper");
 const P = new Pokedex.Pokedex();
+const nationalPMDexCount = 151;
 
 const App = () => {
   // const [pmName, setPmName] = useState(null);
   // const [pmID, setPmID] = useState(null);
   // const [pmSpritesURL, setPmSpritesURL] = useState(null);
-  const [pm, setPM] = useState([]);
-
-  // TODO: use pokeapi-js-wrapper to cache pmdex locally
-  // https://github.com/PokeAPI/pokeapi-js-wrapper
+  const [pms, setPMs] = useState([]);
 
   useEffect(() => {
-    // // Hardcoded for now
-    // fetch("https://pokeapi.co/api/v2/pokemon/778")
-    // .then(results => results.json())
-    //   .then(data => {
-    //     setPmID(data.id);
-    //     setPmName(data.species.name);
-    //     setPmSpritesURL(data.sprites.front_default);
-    //   });
-
-    // P.getPokemonByName("mimikyu-disguised")
-    // .then(response => {
-    //   setPmID(response.id);
-    //   setPmName(response.species.name);
-    //   setPmSpritesURL(response.sprites.front_default);
-    //   console.log(response)
-    // });
-
-    const interval = {
-      offset: 24,
-      limit: 10,
-    };
+    // const interval = {
+    //   offset: 0,
+    //   limit: 2,
+    // };
 
     async function fetchPokemon() {
-      let response = await P.getPokemonsList(interval);
-      // response = await response.json();
-      setPM(response.results);
-      // console.log(response.results)
-      // setPmID(response.id);
-      // setPmName(response.species.name);
-      // setPmSpritesURL(response.sprites.front_default);
+      let indexArr = [ ...Array(nationalPMDexCount).keys() ].map( i => i+1);
+      try {
+        // P.getPokemonByName(array_of_ids), it returns a Promise.all(),
+        // which is all or nothing. It resolves once all promises in the array resolve,
+        // or reject as soon as one of them rejects.
+        return await P.getPokemonByName(indexArr);
+      } catch (e) {
+        console.error(e);
+      } finally {
+        console.log("balabalbalbala");
+      }
+      return "Error!!!!!!";
+    };
+
+    async function run() {
+      let response = await fetchPokemon();
+      console.log(response);
+      setPMs(response);
     }
-    fetchPokemon();
+    run();
   }, []); // <-- Have to pass in [] here!
 
   return (
@@ -56,7 +48,7 @@ const App = () => {
       <Pokeball />
       <h3> Pokemon lists from #26 - #45</h3>
       {/* <PokemonItem pm_name={pmName} pm_sprite_url={pmSpritesURL} pm_id={pmID}></PokemonItem> */}
-      <PokemonList results={pm} P={P}></PokemonList>
+      <PokemonList results={pms} P={P}></PokemonList>
     </div>
   );
 };
