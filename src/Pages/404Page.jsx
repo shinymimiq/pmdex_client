fe93d58b434f8c1cf05619cf60b4feed6e16374c
Jeffrey <77704367+fengbanjiazhu@users.noreply.github.com>
2021-04-20 17:15:45 +1000

    changed style for 404 page

--- src/Pages/404Page.jsx
+++ src/Pages/404Page.jsx
@@ -1,9 +1,12 @@
 import React from 'react'
+import './404Page.css'
+import DEADLINK from '../Pages/deadlink.png'
 
 const NotFoundPage = () => {
   return (
     <div className="page_not_found">
-      Page Not Found
+      you found a <p>DEAD LINK!</p>
+    <img src={DEADLINK} alt=""/>
     </div>
   )
 }
