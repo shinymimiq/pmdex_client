a4b6fb17a754c1bef7bbaf1a37cc258bc614da71
Shiny Mimikyu <mrtoniliu@gmail.com>
2021-05-08 02:42:29 +0100

    update

--- src/Components/PokemonView/abilities/PokemonAbilities.jsx
+++ src/Components/PokemonView/abilities/PokemonAbilities.jsx
@@ -1,12 +1,12 @@
 import React from "react";
 
-import { useState, useEffect } from "react";
-import apiGen from "../../../Api/apiGen.js";
+// import { useState, useEffect } from "react";
+// import apiGen from "../../../Api/apiGen.js";
 
 import "./PokemonAbilities.css";
 
 export const PokemonAbilities = ({ ab }) => {
-  const [abilities, setAbilities] = useState();
+  // const [abilities, setAbilities] = useState();
 
   const abilities_string = ab.map((a) => (
     <span key={a.ability.name} className="pm-ability-string">
@@ -17,23 +17,24 @@ export const PokemonAbilities = ({ ab }) => {
 
   // TODO: Can I make this function reuseable via custom hooks?
   //       It is been used across different components
-  useEffect(() => {
-    const getAbility = async () => {
-      return Promise.all(
-        ab.map((a) => apiGen.getAbilityByName(a.ability.name))
-      );
-    };
 
-    async function run() {
-      try {
-        let res = await getAbility();
-        setAbilities(res);
-      } catch (e) {
-        console.error(e);
-      }
-    }
-    run();
-  }, [ab]);
+  // useEffect(() => {
+  //   const getAbility = async () => {
+  //     return Promise.all(
+  //       ab.map((a) => apiGen.getAbilityByName(a.ability.name))
+  //     );
+  //   };
+
+  //   async function run() {
+  //     try {
+  //       let res = await getAbility();
+  //       setAbilities(res);
+  //     } catch (e) {
+  //       console.error(e);
+  //     }
+  //   }
+  //   run();
+  // }, [ab]);
 
   return <div className="pokemon-ability">{abilities_string}</div>;
 };
